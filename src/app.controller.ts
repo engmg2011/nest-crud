4007c8a23ef40acebb1471a9ae0497799b7a8b4d
Mohamed Gamal <eng.mg2011@gmail.com>
2023-01-18 17:59:30 +0300

    refactor: created auth controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,26 +1,13 @@
 import { AppService } from './app.service';
-import { Controller,Get, Request, Post, UseGuards } from '@nestjs/common';
-import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { Controller,Get} from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService , private authService: AuthService) {}
+  constructor(private readonly appService: AppService ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
-  @Post('auth/login')
-  async login(@Request() req) {
-    console.log("{email: req.email, password: req.password}", req.body)
-    return this.authService.login(req.body);
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
-  }
 }
