b10d2daefa8ed425fbb3ac475484427a3e1fa6ba
Mohamed Gamal <eng.mg2011@gmail.com>
2023-01-18 18:25:07 +0300

    feat: added validation to user creation

--- src/users/dtos/CreateUser.dto.ts
+++ src/users/dtos/CreateUser.dto.ts
@@ -1,6 +1,17 @@
+import {IsEmail, IsNotEmpty, MinLength} from "class-validator";
+import {Match} from "../../decorators/match.decorator";
+
 export class CreateUserDto{
+    @IsNotEmpty()
+    @MinLength(4)
     username: string;
+    @IsNotEmpty()
+    @MinLength(6)
     password: string;
+    @IsEmail()
+    @IsNotEmpty()
     email: string;
+    @IsNotEmpty()
+    @Match('password')
     confirmPassword: string;
 }
